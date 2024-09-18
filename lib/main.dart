da3c715bf2aca2a631282945b7dbf5cd0b116d23
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-18 15:54:27 +0700

    feat: Add Firebase initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:wibu_verse/core/injection/injection.dart';
 import 'package:wibu_verse/src/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initDependencies();
   runApp(const App());
 }
