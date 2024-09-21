b66399a497bc0463ed20031acb7fc7f3dcecf4ff
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-21 22:26:43 +0700

    refactor: Update home_controller.dart to fetch and store anime data
    This commit updates the home_controller.dart file to include functions for fetching and storing recommendation anime and top anime data. The functions make use of the Dio package for making HTTP requests and handle error cases. The fetched data is then processed and stored in appropriate model classes. This refactor improves the code by separating the data fetching logic from the controller class.
    
    Refactor the home_controller.dart file to fetch and store anime data

--- lib/src/home/controller/home_controller.dart
+++ lib/src/home/controller/home_controller.dart
@@ -1,3 +1,92 @@
+import 'package:dio/dio.dart';
+import 'package:wibu_verse/app_logger.dart';
+import 'package:wibu_verse/core/di/injection.dart';
+import 'package:wibu_verse/core/utils/helper_variabel/helper_variabel.dart';
+import 'package:wibu_verse/src/home/model/recommendation_anime.dart';
+import 'package:wibu_verse/src/home/model/top_anime.dart';
+
 class HomeController {
   HomeController();
+  final _dio = getIt<Dio>();
+
+  // Get Recommendation anime
+  Future<List<RecommendationAnime>> getRecommendationsAnime() async {
+    try {
+      final _response = await _dio.get(HelperVariabel.recomAnime);
+
+      if (_response.statusCode == 200) {
+        Map<String, dynamic> responseData =
+            _response.data as Map<String, dynamic>;
+
+        List<dynamic> recommendationJsonList =
+            responseData["data"] as List<dynamic>;
+
+        List<RecommendationAnime> result = [];
+
+        for (var anime in recommendationJsonList) {
+          for (var entry in anime['entry']) {
+            if (entry['mal_id'] != null &&
+                entry['title'] != bool &&
+                entry['images']['jpg']['image_url'] != null) {
+              result.add(RecommendationAnime(
+                malId: entry['mal_id'],
+                title: entry['title'] ?? 'Unknown Title',
+                imageUrl: entry['images']['jpg']['image_url'] ?? '',
+              ));
+            }
+          }
+        }
+
+        return result;
+      } else {
+        AppLogger.error(
+            "Failed to fetch recommendations. Status code: ${_response.statusCode}");
+        return [];
+      }
+    } catch (error, stacktrace) {
+      AppLogger.error("Error occurred during fetching recommendations: $error");
+      AppLogger.trace("Stacktrace: $stacktrace");
+
+      return [];
+    }
+  }
+
+  // get Top Anime
+  Future<List<TopAnime>> getTopAnime() async {
+    try {
+      final _response = await _dio.get(HelperVariabel.topAnime);
+
+      if (_response.statusCode == 200) {
+        Map<String, dynamic> responseData =
+            _response.data as Map<String, dynamic>;
+
+        List<dynamic> topAnimeJsonList = responseData["data"] as List<dynamic>;
+
+        List<TopAnime> result = [];
+
+        for (var anime in topAnimeJsonList) {
+          result.add(
+            TopAnime(
+              malId: anime['mal_id'].toString(),
+              title: anime['title'] ?? 'Unknown Title',
+              imageUrl: anime['images']['jpg']['large_image_url'] ?? '',
+              score: anime['score'] ?? 0.0,
+              rank: anime['rank'] ?? 0,
+            ),
+          );
+        }
+
+        return result;
+      } else {
+        AppLogger.error(
+            "Failed to fetch top anime. Status code: ${_response.statusCode}");
+        return [];
+      }
+    } catch (error, stacktrace) {
+      AppLogger.error("Error occurred during fetching top anime: $error");
+      AppLogger.trace("Stacktrace: $stacktrace");
+
+      return [];
+    }
+  }
 }
