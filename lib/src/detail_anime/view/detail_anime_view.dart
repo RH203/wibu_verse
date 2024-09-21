efec412bbb113d97a2ce699036397e9fcd031183
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-21 22:26:39 +0700

    feat: Add DetailAnimeView class for displaying detailed anime information

---
+++ lib/src/detail_anime/view/detail_anime_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailAnimeView extends StatelessWidget {
+  const DetailAnimeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
