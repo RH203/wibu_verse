7feb327f741ee9e94b589a455d9bca23fe7b2aa8
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-21 22:26:11 +0700

    feat: Add dependency injection for Dio and FirebaseAuth
    This commit adds a new file, `injection.dart`, which sets up dependency injection for the `Dio` HTTP client and `FirebaseAuth` instance. It also registers the `AppRouter` singleton from the `core/router/router.dart` file.
    
    The `Dio` instance is created and registered using the `Dio()` constructor.
    
    This commit addresses the need for dependency injection in the project and improves code organization and reusability.

---
+++ lib/core/di/injection.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:wibu_verse/core/router/router.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initDependencies() async {
+  getIt.registerSingleton<AppRouter>(AppRouter());
+  getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+
+  Dio dio = Dio();
+
+  getIt.registerSingleton<Dio>(dio);
+}
