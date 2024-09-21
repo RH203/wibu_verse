22d69b00d3b2472edeb7eaf0237b5440b31d436c
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-21 22:26:18 +0700

    refactor: Update import statement for dependency injection in app.dart

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:wibu_verse/core/injection/injection.dart';
+import 'package:wibu_verse/core/di/injection.dart';
 import 'package:wibu_verse/core/router/router.dart';
 
 class App extends StatelessWidget {
